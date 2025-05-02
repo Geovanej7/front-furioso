3602c6322fd4f905ea2c352c0cb219bc624f73b7
Geo <gjsj@discente.ifpe.edu.br>
2025-05-02 18:08:55 -0300

    add css forms

--- src/app/pages/login-page/login/login.component.ts
+++ src/app/pages/login-page/login/login.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  formLogin: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.formLogin = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  get email() {
+    return this.formLogin.get('email')!;
+  }
+
+  get senha() {
+    return this.formLogin.get('senha')!;
+  }
+
+  onSubmit() {
+    if (this.formLogin.valid) {
+      console.log('Formulário enviado', this.formLogin.value);
+    }
+  }
 }
